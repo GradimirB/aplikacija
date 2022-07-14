96844d8a6674f50547e5120c9273fbef5975ee92
Gradimir <gradimirbabic2@gmail.com>
2022-07-14 19:16:14 +0200

    implement image resizing mechanism

--- src/controllers/api/article.contoller.ts
+++ src/controllers/api/article.contoller.ts
@@ -9,8 +9,13 @@ import {diskStorage} from "multer";
 import { PhotoService } from "src/services/photo/photo.service";
 import { Photo } from "entities/photo.entity";
 import { ApiResponse } from "src/misc/api.response.class";
+//import * as fileType from 'file-type'; 
+import * as fs from 'fs';
+import * as sharp from 'sharp';
 
 
+const fileType = require("file-type");
+ 
 @Controller('api/article')
 @Crud({
     model:{
@@ -55,81 +60,114 @@ export class ArticleController{
         return this.service.createFullArticle(data);
     }
 
-    @Post(':id/uploadPhoto/')
+    @Post(':id/uploadPhoto/') // POST http://localhost:3000/api/article/:id/uploadPhoto/
     @UseInterceptors(
         FileInterceptor('photo', {
             storage: diskStorage({
-                destination:StorageConfig.photosDestination,
-                filename:(req,file, callback) =>{
-                    let original = file.originalname;
+                destination: StorageConfig.photo.destination,
+                filename: (req, file, callback) => {
+                    let original: string = file.originalname;
 
                     let normalized = original.replace(/\s+/g, '-');
                     normalized = normalized.replace(/[^A-z0-9\.\-]/g, '');
                     let sada = new Date();
                     let datePart = '';
-                    datePart += sada.getFullYear();
-                    datePart += (sada.getMonth()+1).toString();
+                    datePart += sada.getFullYear().toString();
+                    datePart += (sada.getMonth() + 1).toString();
                     datePart += sada.getDate().toString();
 
-                     
-                    let randomPart:string =
+                    let randomPart: string =
                         new Array(10)
                             .fill(0)
-                            .map(e => (Math.random()*9).toFixed(0).toString())
+                            .map(e => (Math.random() * 9).toFixed(0).toString())
                             .join('');
 
-                    let fileName = datePart + '-' + randomPart + '-' +normalized;
-
+                    let fileName = datePart + '-' + randomPart + '-' + normalized;
                     fileName = fileName.toLocaleLowerCase();
 
-                    callback(null, fileName); 
+                    callback(null, fileName);
                 }
             }),
-            fileFilter:(req,file,callback) => {
-                if(!file.originalname.match(/\.(jpg|png)$/)){
-                    req.fileFilterError = "Bad file extension!";
-                    callback(null,false);
+            fileFilter: (req, file, callback) => {
+                // 1. Check ekstenzije: JPG, PNG
+                if (!file.originalname.toLowerCase().match(/\.(jpg|png)$/)) {
+                    req.fileFilterError = 'Bad file extension!';
+                    callback(null, false);
                     return;
                 }
 
-                if(!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))){
-                    req.fileFilterError = "Bad file extension!";
-                    callback(null,false);
+                // 2. Check tipa sadrzaja: image/jpeg, image/png (mimetype)
+                if (!(file.mimetype.includes('jpeg') || file.mimetype.includes('png'))) {
+                    req.fileFilterError = 'Bad file content type!';
+                    callback(null, false);
                     return;
                 }
 
-                callback(null,true);
+                callback(null, true);
             },
-            limits:{
-                files:1,
-                fileSize:StorageConfig.photoMaxFileSize,
+            limits: {
+                files: 1,
+                fileSize: StorageConfig.photo.maxSize,
             },
         })
     )
     async uploadPhoto(
-        @Param('id') articleId:number, 
+        @Param('id') articleId: number,
         @UploadedFile() photo,
         @Req() req
-        ):Promise <ApiResponse | Photo>{
-        if(req.fileFilterError){
+    ): Promise<ApiResponse | Photo> {
+        if (req.fileFilterError) {
             return new ApiResponse('error', -4002, req.fileFilterError);
         }
 
-        if(!photo){
+        if (!photo) {
             return new ApiResponse('error', -4002, 'File not uploaded!');
         }
-             
-        const newPhoto:Photo = new Photo();
+
+        const fileTypeResult = await fileType.fromFile(photo.path);
+        if (!fileTypeResult) {
+            fs.unlinkSync(photo.path);
+            return new ApiResponse('error', -4002, 'Cannot detect file type!');
+        }
+
+        const realMimeType = fileTypeResult.mime;
+        if (!(realMimeType.includes('jpeg') || realMimeType.includes('png'))) {
+            fs.unlinkSync(photo.path);
+            return new ApiResponse('error', -4002, 'Bad file content type!');
+        }
+
+        await this.createResizedImage(photo, StorageConfig.photo.resize.thumb);
+        await this.createResizedImage(photo, StorageConfig.photo.resize.small);
+
+        const newPhoto: Photo = new Photo();
         newPhoto.articleId = articleId;
         newPhoto.imagePath = photo.filename;
 
-        const savedPhoto =  await this.photoService.add(newPhoto);
-        if(!savedPhoto){
+        const savedPhoto = await this.photoService.add(newPhoto);
+        if (!savedPhoto) {
             return new ApiResponse('error', -4001);
         }
 
-        return savedPhoto
+        return savedPhoto;
     }
 
+    async createResizedImage(photo, resizeSettings) {
+        const originalFilePath = photo.path;
+        const fileName = photo.filename;
 
+        const destinationFilePath =
+            StorageConfig.photo.destination +
+            resizeSettings.directory +
+            fileName;
+
+        await sharp(originalFilePath)
+            .resize({
+                fit: 'cover',
+                width: resizeSettings.width,
+                height: resizeSettings.height,
+            })
+            .toFile(destinationFilePath);
+    }
+
+   
 }
