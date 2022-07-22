7a88946aed5518a7847daa4bfcbcc85cde5313ee
Gradimir <gradimirbabic2@gmail.com>
2022-07-22 22:32:24 +0200

    git commit -m "Implement article editing features"

--- src/controllers/api/article.contoller.ts
+++ src/controllers/api/article.contoller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Param, Post, Req, UploadedFile, UseInterceptors} from "@nestjs/common";
+import { Body, Controller, Delete, Param, Patch, Post, Req, UploadedFile, UseInterceptors} from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { Crud } from "@nestjsx/crud";
 import { StorageConfig } from "config.ts/storage.config";
@@ -12,6 +12,9 @@ import { ApiResponse } from "src/misc/api.response.class";
 //import * as fileType from 'file-type'; 
 import * as fs from 'fs';
 import * as sharp from 'sharp';
+import { EditAdministratorDto } from "src/dtos/administrator/edit.administrator.dto";
+import { EditArticleDto } from "src/dtos/article/edit.article.dto";
+import { ArticlePrice } from "src/entities/article-price.entity";
 
 
 const fileType = require("file-type");
@@ -47,6 +50,10 @@ const fileType = require("file-type");
             }
              
         }
+    },
+
+    routes:{
+        exclude: ['updateOneBase','replaceOneBase','deleteOneBase'],
     }
 })
 export class ArticleController{
@@ -58,6 +65,11 @@ export class ArticleController{
     @Post('createFull')
     createFullArticle(@Body() data:AddArticleDto){
         return this.service.createFullArticle(data);
+    }
+
+    @Patch(':id')
+    editFullArticle(@Param('id') id:number, @Body() data:EditArticleDto){
+        return this.service.editFullArticle(id, data); 
     }
 
     @Post(':id/uploadPhoto/') // POST http://localhost:3000/api/article/:id/uploadPhoto/
